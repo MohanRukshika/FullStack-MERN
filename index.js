d9535484e28e09d8f09337845709d27c4edcb82b
RukshikaU1 <mohanrukshika054@gmail.com>
2026-06-16 22:21:37 +0530

    Ignore node_modules

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import express from 'express'
 
 let app = express()
 
-function go(){
-    console.log("started...")
-}
-
-app.listen(3000,go)
+app.listen(3000,()=>{
+    console.log("Server is starting.....");
+})
