2c2937f5b6d80d1d25d5f4234cd42b9a2f0ca50a
RukshikaU1 <mohanrukshika054@gmail.com>
2026-06-18 09:46:02 +0530

    r

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import express from 'express'
 
-let app = express()
+const app = express()
+
+app.use( express.json() )
+
+app.put("/",()=>{
+    console.log("Put Request Received")
+})
+
+app.get("/",(req,res)=>{
+    console.log("Get Request Received")
+    console.log(req.body)
+    res.json({
+        "message":"data received successfully"
+    })
+})
 
 app.listen(3000,()=>{
     console.log("Server is starting.....");
